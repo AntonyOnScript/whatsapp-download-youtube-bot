a4cc81065975cba4b3da5e624be666a97bbe1a18
AntonyOnScript <santhiagoafk@gmail.com>
2022-09-02 20:35:05 -0300

    feat (draft): download youtube method

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM public.ecr.aws/lambda/nodejs:16
 COPY app.js package.json  ${LAMBDA_TASK_ROOT}/
 RUN npm i
+COPY . .
 CMD [ "app.handler" ]  
--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const responseBuilder = require("aws-lambda-response-builder")
-const okResponse = responseBuilder.buildApiGatewayOkResponse({
-    message: "some message"
-}) 
+const fs = require('fs')
+const youtubedl = require('youtube-dl')
 
-exports.handler = (req, res) => {
-    console.log(req)
-    return okResponse
+exports.handler = async (event, context) => {
+    const response = responseBuilder.buildApiGatewayOkResponse()
+    console.log(JSON.stringify(event))
+    console.log(JSON.stringify(context))
+
+    if (event.httpMethod === "GET") {
+        if (event.queryStringParameters?.ytbUrl) {
+            const video = youtubedl(event.queryStringParameters?.ytbUrl,
+            // Optional arguments passed to youtube-dl.
+            ['--format=18'],
+            // Additional options can be given for calling `child_process.execFile()`.
+            { cwd: __dirname })
+            console.log(video)
+            // Will be called when the download starts.
+            video.on('info', function(info) {
+                console.log(info)
+                console.log('Download started')
+                console.log('filename: ' + info._filename)
+                console.log('size: ' + info.size)
+            })
+
+            // video.pipe(fs.createWriteStream('myvideo.mp4'))
+        }
+    }
+    return response
 }
