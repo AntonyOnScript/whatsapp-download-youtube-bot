bab3c5474082449ea96c721b28523d2e2102f61e
AntonyOnScript <santhiagoafk@gmail.com>
2022-09-10 16:25:25 -0300

    feat: download youtube videos function and add dev files (serverless.yml, dockerfile)

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM public.ecr.aws/lambda/nodejs:16
 COPY app.js package.json  ${LAMBDA_TASK_ROOT}/
+ENV bucket=${bucket}
+ENV accessKeyId=${accessKeyId}
+ENV secretAccessKey=${secretAccessKey}
 RUN npm i
 COPY . .
 CMD [ "app.handler" ]  
--- app.js
+++ app.js
@@ -1,30 +1,76 @@
 const responseBuilder = require("aws-lambda-response-builder")
+const ytdl = require("ytdl-core")
 const fs = require('fs')
-const youtubedl = require('youtube-dl')
+const AWS = require('aws-sdk')
+const { accessKeyId, secretAccessKey, bucket } = process.env
+const S3 = new AWS.S3({
+    s3ForcePathStyle: true,
+    accessKeyId,
+    secretAccessKey
+})
+
+async function generateVideo(url, title) {
+    return await new Promise(async (resolve, reject) => {
+        const dir = `${__dirname}/videos/${title}.mp4`
+        try {
+            await new Promise((resolve, reject) => {
+                fs.stat(dir, (err, stats) => {
+                    console.log(err, stats)
+                    if (stats)
+                        return reject('file already exists')
+                    return resolve('ok')
+                })
+            })
+            const writter = fs.createWriteStream(dir)
+            const streamYtb = ytdl(url).pipe(writter)
+            await new Promise((resolve, reject) => {
+                streamYtb.on('finish', async () => {
+                    const fileStream = fs.createReadStream(dir)
+                    await new Promise((resolve) => {
+                        S3.putObject({
+                            Bucket: bucket,
+                            Key: `${title}.mp4`,
+                            Body: fileStream,
+                            ContentType: 'video/mp4'
+                        }, (err, data) => {
+                            console.log(err, data)
+                            resolve()
+                        })
+                    })
+                    resolve()
+                })
+            })
+            resolve()
+        } catch (err) {
+            const fileStream = fs.createReadStream(dir)
+            console.log(fileStream)
+            await new Promise((resolve) => {
+                S3.putObject({
+                    Bucket: bucket,
+                    Key: `${title}-4.mp4`,
+                    Body: fileStream,
+                    ContentType: 'video/mp4'
+                }, (err, data) => {
+                    console.log(err, data)
+                    resolve()
+                })
+            })
+            return reject(err)
+        }
+    })
+}
 
 exports.handler = async (event, context) => {
-    const response = responseBuilder.buildApiGatewayOkResponse()
-    console.log(JSON.stringify(event))
-    console.log(JSON.stringify(context))
-
-    if (event.httpMethod === "GET") {
-        if (event.queryStringParameters?.ytbUrl) {
-            const video = youtubedl(event.queryStringParameters?.ytbUrl,
-            // Optional arguments passed to youtube-dl.
-            ['--format=18'],
-            // Additional options can be given for calling `child_process.execFile()`.
-            { cwd: __dirname })
-            console.log(video)
-            // Will be called when the download starts.
-            video.on('info', function(info) {
-                console.log(info)
-                console.log('Download started')
-                console.log('filename: ' + info._filename)
-                console.log('size: ' + info.size)
-            })
-
-            // video.pipe(fs.createWriteStream('myvideo.mp4'))
+    const response = responseBuilder.buildApiGatewayOkResponse({ message: "ok" })
+    const videoLink = event.queryStringParameters?.v
+    if (videoLink) {
+        const isTrustedLink = ytdl.validateURL(videoLink)
+        if (isTrustedLink) {
+            const data = await ytdl.getInfo(videoLink)
+            const title = data.videoDetails.title
+            await generateVideo(videoLink, title)
         }
     }
+
     return response
 }
