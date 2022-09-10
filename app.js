d51443cbe4d2e94eaa2f92b9e1ca25cb56e21e6b
AntonyOnScript <santhiagoafk@gmail.com>
2022-09-10 18:04:11 -0300

    feat: add conditional S3 upload

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const S3 = new AWS.S3({
 async function generateVideo(url, title) {
     return await new Promise(async (resolve, reject) => {
         const dir = `${__dirname}/videos/${title}.mp4`
+        const S3Url = `https://${bucket}.s3.amazonaws.com/${encodeURIComponent(title).replaceAll('%20', '+')}.mp4`
+        if(!fs.existsSync(`${__dirname}/videos/`)) fs.mkdirSync(`${__dirname}/videos/`)
         try {
             await new Promise((resolve, reject) => {
                 fs.stat(dir, (err, stats) => {
@@ -21,9 +23,25 @@ async function generateVideo(url, title) {
                     return resolve('ok')
                 })
             })
+            
+            try {
+                await new Promise((resolve, reject) => {
+                    S3.getObject({
+                        Bucket: bucket,
+                        Key: `${title}.mp4`
+                    }, (err, data) => {
+                        console.log(err, data)
+                        if (err) resolve()
+                        if (data) reject(S3Url) 
+                    })
+                })
+            } catch(e) {
+                throw new Error(e)
+            }
+            
             const writter = fs.createWriteStream(dir)
             const streamYtb = ytdl(url).pipe(writter)
-            await new Promise((resolve, reject) => {
+            await new Promise((resolve) => {
                 streamYtb.on('finish', async () => {
                     const fileStream = fs.createReadStream(dir)
                     await new Promise((resolve) => {
@@ -38,37 +56,27 @@ async function generateVideo(url, title) {
                         })
                     })
                     resolve()
+                    fs.rmSync(dir)
                 })
             })
-            resolve()
+            resolve(S3Url)
         } catch (err) {
-            const fileStream = fs.createReadStream(dir)
-            console.log(fileStream)
-            await new Promise((resolve) => {
-                S3.putObject({
-                    Bucket: bucket,
-                    Key: `${title}-4.mp4`,
-                    Body: fileStream,
-                    ContentType: 'video/mp4'
-                }, (err, data) => {
-                    console.log(err, data)
-                    resolve()
-                })
-            })
-            return reject(err)
+            console.log(err)
+            return reject(err.message)
         }
     })
 }
 
 exports.handler = async (event, context) => {
-    const response = responseBuilder.buildApiGatewayOkResponse({ message: "ok" })
     const videoLink = event.queryStringParameters?.v
+    let response = {}
     if (videoLink) {
         const isTrustedLink = ytdl.validateURL(videoLink)
         if (isTrustedLink) {
             const data = await ytdl.getInfo(videoLink)
             const title = data.videoDetails.title
-            await generateVideo(videoLink, title)
+            const link = await generateVideo(videoLink, title)
+            response = responseBuilder.buildApiGatewayOkResponse({ message: link })
         }
     }
 
